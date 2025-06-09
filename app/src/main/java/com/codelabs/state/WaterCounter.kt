7b763401dcebc40d7f042c847e6a04ca724125d0
Valeryo Salim <valeryoks55@gmail.com>
2025-06-09 21:33:31 +0700

    Memory in a composable function

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -28,6 +28,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
@@ -58,7 +60,8 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
